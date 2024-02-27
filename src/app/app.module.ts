2dcea382462efe51eecb0f995b2dded696ac4c42
Mário Pontes <mario.canys@hotmail.com>
2024-02-27 10:11:18 -0300

    Criação base da estrutura: rotas, components e pages

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { CardComponent } from './components/card/card.component';
+import { HomeComponent } from './pages/home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    CardComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
